64ebb1f4bf0943cf750492c04afa150ced2fffef
Yurij <nabor_2008@i.ua>
2020-07-21 16:13:51 +0300

    add category controller

--- public_html/server/views/home/index.php
+++ public_html/server/views/home/index.php
@@ -29,14 +29,14 @@
         <div class="col-lg-4 col-md-6 mb-4">
             <div class="card h-100">
                 <a href="<?= \yii\helpers\Url::to(['product/view', 'id' => $product->id]) ?>">
-                    <?= \yii\helpers\Html::img("@web/game_images/$product[img]", ['alt' => $product['title'], 'class' => 'card-img-top']) ?>
+                    <?= \yii\helpers\Html::img("@web/game_images/{$product->img}", ['alt' => $product->title, 'class' => 'card-img-top']) ?>
                 </a>
                 <div class="card-body">
                     <h5 class="card-title">
                         <a href="<?= \yii\helpers\Url::to(['product/view', 'id' => $product->id]) ?>"><?= $product->title ?></a>
                     </h5>
                     <h5></h5>
-                    <p class="card-text">555</p>
+                    <p class="card-text"><?= $product->price ?></p>
                 </div>
                 <div class="card-footer">
                     <a href="/cart/add/ttttt"
--- public_html/server/views/site/error.php
+++ public_html/server/views/site/error.php
@@ -3,6 +3,7 @@
 /* @var $this yii\web\View */
 /* @var $name string */
 /* @var $message string */
+
 /* @var $exception Exception */
 
 use yii\helpers\Html;
@@ -10,18 +11,8 @@ use yii\helpers\Html;
 $this->title = $name;
 ?>
 <div class="site-error">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-
+    <h1 class="my-3"><?= Html::encode($this->title) ?></h1>
     <div class="alert alert-danger">
         <?= nl2br(Html::encode($message)) ?>
     </div>
-
-    <p>
-        The above error occurred while the Web server was processing your request.
-    </p>
-    <p>
-        Please contact us if you think this is a server error. Thank you.
-    </p>
-
 </div>
