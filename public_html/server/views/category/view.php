a0c47f3f5f788867716d78d11019eb2173b8797d
Yurij2015 <nabor_2008@i.ua>
2020-07-21 22:39:29 +0300

    create setMeta function

--- public_html/server/views/category/view.php
+++ public_html/server/views/category/view.php
@@ -1,24 +1,35 @@
-<h3 class="my-3">Товары категории: <?=$category['title']?></h3>
+<?php
+
+use yii\helpers\Html;
+use yii\helpers\Url;
+
+?>
+<h3 class="my-3">Товары категории: <?= $category['title'] ?></h3>
 <div class="row">
-    <?php foreach ($products as $product): ?>
-        <div class="col-lg-4 col-md-6 mb-4">
-            <div class="card h-100">
-                <a href="<?= \yii\helpers\Url::to(['product/view', 'id' => $product->id]) ?>">
-                    <?= \yii\helpers\Html::img("@web/game_images/{$product->img}", ['alt' => $product->title, 'class' => 'card-img-top']) ?>
-                </a>
-                <div class="card-body">
-                    <h5 class="card-title">
-                        <a href="<?= \yii\helpers\Url::to(['product/view', 'id' => $product->id]) ?>"><?= $product->title ?></a>
-                    </h5>
-                    <h5></h5>
-                    <p class="card-text"><?= $product->price ?></p>
-                </div>
-                <div class="card-footer">
-                    <a href="/cart/add/ttttt"
-                       class="btn btn-outline-primary btn-sm float-right">В корзину</a>
+    <?php if (!empty($products)) : ?>
+        <?php foreach ($products as $product): ?>
+            <div class="col-lg-4 col-md-6 mb-4">
+                <div class="card h-100">
+                    <a href="<?= Url::to(['product/view', 'id' => $product->id]) ?>">
+                        <?= Html::img("@web/game_images/{$product->img}", ['alt' => $product->title, 'class' => 'card-img-top']) ?>
+                    </a>
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            <a href="<?= Url::to(['product/view', 'id' => $product->id]) ?>"><?= $product->title ?></a>
+                        </h5>
+                        <p class="card-text"><?= $product->price ?></p>
+                    </div>
+                    <div class="card-footer">
+                        <a href="/cart/add/ttttt"
+                           class="btn btn-outline-primary btn-sm float-right">В корзину</a>
+                    </div>
                 </div>
             </div>
+        <?php endforeach; ?>
+    <?php else: ?>
+        <div class="col-lg-12 col-md-12 mb-12">
+            <h5>В этой категории еще нет игр!</h5>
         </div>
-    <?php endforeach; ?>
+    <?php endif; ?>
 </div>
 <!-- /.row -->
--- public_html/server/views/layouts/sidebar.php
+++ public_html/server/views/layouts/sidebar.php
@@ -1,6 +1,11 @@
+<?php
+
+use app\components\MenuWidget;
+
+?>
 <h3 class="my-3">Категории</h3>
 <div class="list-group">
-    <?= \app\components\MenuWidget::widget([
+    <?= MenuWidget::widget([
         'tpl' => 'menu'
     ]) ?>
 </div>
